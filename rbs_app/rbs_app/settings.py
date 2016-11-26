42ddfcce3ec5c5ee6067839a6efed49173193d8b
jnthnrzr <jonathanrozario@gmail.com>
2016-11-25 23:59:40 -0500

    admin site

--- rbs_app/rbs_app/settings.py
+++ rbs_app/rbs_app/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import django.contrib.admin
 
+
+django.contrib.admin.LOGIN_URL = '/login'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
