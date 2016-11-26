d59098dabc43a5dcfee1d82d749afc32f2268528
jnthnrzr <jonathanrozario@gmail.com>
2016-11-26 00:54:08 -0500

    login not yet working

--- rbs_app/rbs_app/settings.py
+++ rbs_app/rbs_app/settings.py
@@ -14,7 +14,7 @@ import os
 import django.contrib.admin
 
 
-django.contrib.admin.LOGIN_URL = '/login'
+django.contrib.admin.LOGIN_URL = 'login/'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
