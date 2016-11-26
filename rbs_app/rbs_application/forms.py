0547a5e41cfcba66191527e7ce3017031d5de380
jnthnrzr <jonathanrozario@gmail.com>
2016-11-26 17:05:07 -0500

    testing suspicious registration

--- rbs_app/rbs_application/forms.py
+++ rbs_app/rbs_application/forms.py
@@ -3,12 +3,24 @@ from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+''' CONNIE'S CODE
 class RegistrationForm(forms.Form):
 
-    username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
-    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
+    username = forms.RegexField(
+    regex=r'^\w+$',
+    widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"),
+    error_messages={ 'invalid':_("This value must contain only letters, numbers and underscores.") })
+
+    email = forms.EmailField(
+    widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
+
+    password1 = forms.CharField(
+    widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)),
+    label=_("Password"))
+
+    password2 = forms.CharField(
+    widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)),
+    label=_("Password (again)"))
 
     def clean_username(self):
         try:
@@ -22,3 +34,40 @@ class RegistrationForm(forms.Form):
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
                 raise forms.ValidationError(_("Passwords did not match! Please Try Again."))
         return self.cleaned_data
+'''
+class UserForm(forms.Form):
+
+    username = forms.RegexField(
+    regex=r'^\w+$',
+    widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"),
+    error_messages={ 'invalid':_("This value must contain only letters, numbers and underscores.") })
+
+    email = forms.EmailField(
+    widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("PASSWORD"))
+
+    def clean_username(self):
+        try:
+            user = User.objects.get(username__iexact=self.cleaned_data['username'])
+        except User.DoesNotExist:
+            return self.cleaned_data['username']
+        raise forms.ValidationError(_("The username already exists. Please try another one."))
+
+    def clean(self):
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+                raise forms.ValidationError(_("Passwords did not match! Please Try Again."))
+        return self.cleaned_data
+    # class Meta:
+    #     model = User
+    #     fields = ('username', 'email', 'password')
+
+''' THIS WORKS
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+'''
--- rbs_app/rbs_application/views.py
+++ rbs_app/rbs_application/views.py
@@ -1,13 +1,80 @@
 # Create your views here.
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response, redirect
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
-from .forms import *
+from .forms import UserForm
 # Create your views here.
+
+def register(request):
+    '''signup page view'''
+    # A boolean value for telling the template
+    # whether the registration was successful.
+    # Set to False initially. Code changes value to
+    # True when registration succeeds.
+    registered = False
+
+    # If it's a HTTP POST, we're interested in processing form data.
+    if request.method == 'POST':
+        # Attempt to grab information from the raw form information.
+        # Note that we make use of only the UserForm
+        user_form = UserForm(data=request.POST)
+        # profile_form = UserProfileForm(data=request.POST)
+
+        # If the two forms is valid...
+        if user_form.is_valid():
+            user = User.objects.create_user(
+            username=user_form.cleaned_data['username'],
+            password=user_form.cleaned_data['password'],
+            email=user_form.cleaned_data['email']
+            )
+            user.save()
+
+            # user = user_form.save()
+
+            # Now we hash the password with the set_password method.
+            # Once hashed, we can update the user object.
+            # user.set_password(user.password)
+            # user.save()
+
+            # Now sort out the UserProfile instance.
+            # Since we need to set the user attribute ourselves,
+            # we set commit=False. This delays saving the model
+            # until we're ready to avoid integrity problems.
+            # profile = profile_form.save(commit=False)
+            # profile.user = user
+
+            # Did the user provide a profile picture?
+            # If so, we need to get it from the input form and
+            # put it in the UserProfile model.
+            # if 'picture' in request.FILES:
+                # profile.picture = request.FILES['picture']
+
+            # Now we save the UserProfile model instance.
+            # profile.save()
+
+             # Update our variable to indicate that the template
+             # registration was successful.
+            registered = True
+
+        else:
+            # Invalid form or forms - mistakes or something else?
+            # Print problems to the terminal.
+            print(user_form.errors)
+    else:
+        # Not a HTTP POST, so we render our form using two ModelForm instances. # These forms will be blank, ready for user input.
+        user_form = UserForm()
+        # profile_form = UserProfileForm()
+    # Render the template depending on the context.
+    return render(request, 'registration.html',
+                      {'user_form': user_form,
+                       'registered': registered})
+
+
 
 def redir(request):
     '''redirection'''
@@ -56,37 +123,6 @@ def user_login(request):
 def user_main(request):
     return render(request, 'user_main.html')
 
-# @csrf_protect
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = User.objects.create_user(
-            username=form.cleaned_data['username_id'],
-            password=form.cleaned_data['password1'],
-            email=form.cleaned_data['email']
-            )
-            return HttpResponseRedirect('/')
-    else:
-        form = RegistrationForm()
-    variables = RequestContext(request, { 'form': form })
-
-    return render_to_response('registration.html',variables,)
-
-
-def process_registration(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = User.objects.create_user( username=form.cleaned_data['username_id'], password=form.cleaned_data['password1'],
-            email=form.cleaned_data['email'] )
-            return HttpResponseRedirect('/')
-    else:
-        form = RegistrationForm()
-    variables = RequestContext(request, { 'form': form })
-
-    return render_to_response('registration.html',variables,)
-
 
 def view_previous_orders(request):
     return render(request, 'previous_orders.html')
@@ -96,24 +132,7 @@ def visitors_main(request):
     return render(request, 'visitors_main.html')
 
 
-
-
 # @login_required
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('visitor'))
-
-
-
-# @login_required
-# def home(request):
-#     return render_to_response(
-#     'home.html',
-#     { 'user': request.user }
-#     )
-
-
-# def register_success(request):
-#     return render_to_response(
-#     'registration/success.html',
-#     )
