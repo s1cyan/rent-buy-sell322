60af7165b748a4692c882533aa26f287ec7ef957
jnthnrzr <jonathanrozario@gmail.com>
2016-11-26 01:10:46 -0500

    added file a complaint url

--- rbs_app/rbs_application/urls.py
+++ rbs_app/rbs_application/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^money/', views.add_withdraw, name='add_withdraw'),
     url(r'^cart/', views.cart, name='cart'),
     url(r'^checkout/', views.confirm_checkout, name='checkout'),
+    url(r'^complaint/', views.file_complaint, name='complaint'),
     # url(r'^login/', views.user_login, name='login'),
     url(r'^orders/', views.view_previous_orders, name='previous_orders'),
     url(r'^register/', views.register, name='registration'),
