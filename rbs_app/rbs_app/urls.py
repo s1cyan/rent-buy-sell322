5299ba8791104c69e5c71e32d5189cbde03723df
Claudia Yan <claudiab.yan@gmail.com>
2016-11-25 22:03:21 -0500

    removed some old overlapping stuff

--- rbs_app/rbs_app/urls.py
+++ rbs_app/rbs_app/urls.py
@@ -19,12 +19,7 @@ from django.contrib import admin
 from login.views import *
 
 urlpatterns = [
-    url(r'^rbs_application/', include('rbs_application.urls')),
+    url(r'^', include('rbs_application.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'django.contrib.auth.views.login'),
-    url(r'^home/$', home),
-    url(r'^register/$', register),
-    url(r'^register/success/$', register_success),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', logout_page),
+
 ]
--- rbs_app/rbs_application/views.py
+++ rbs_app/rbs_application/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'rentbuysell/index.html')
+    return render(request, 'visitors_mains.html')
