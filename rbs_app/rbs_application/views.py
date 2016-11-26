df1d8224f8819d70e2c2bdf8ff717fa7c9893db5
jnthnrzr <jonathanrozario@gmail.com>
2016-11-26 00:03:49 -0500

    testing out authentication

--- rbs_app/rbs_application/views.py
+++ rbs_app/rbs_application/views.py
@@ -1,7 +1,72 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# Create your views here.
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response, redirect
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
+from .forms import *
 # Create your views here.
 
+def redir(request):
+    '''redirection'''
+    return redirect('/rbs')
 
-def index(request):
-    return render(request, 'user_item_details.html')
+def add_withdraw(request):
+    return render(request, 'add_withdraw.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def confirm_checkout(request):
+    return render(request, 'confirm_checkout.html')
+
+
+@csrf_protect
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username_id'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/rbs')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, { 'form': form })
+
+    return render_to_response('registration.html',variables,)
+
+
+def view_previous_orders(request):
+    return render(request, 'previous_orders.html')
+
+
+def visitors_main(request):
+    return render(request, 'visitors_main.html')
+
+
+
+
+
+# def logout_page(request):
+#     logout(request)
+#     return HttpResponseRedirect('/')
+
+
+
+# @login_required
+# def home(request):
+#     return render_to_response(
+#     'home.html',
+#     { 'user': request.user }
+#     )
+
+
+# def register_success(request):
+#     return render_to_response(
+#     'registration/success.html',
+#     )
