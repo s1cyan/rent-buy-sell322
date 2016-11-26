17d53e01e422806601277d96be28faffbfdefdeb
jnthnrzr <jonathanrozario@gmail.com>
2016-11-26 00:54:34 -0500

    more urls to go

--- rbs_app/rbs_application/urls.py
+++ rbs_app/rbs_application/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     url(r'^money/', views.add_withdraw, name='add_withdraw'),
     url(r'^cart/', views.cart, name='cart'),
     url(r'^checkout/', views.confirm_checkout, name='checkout'),
-    url(r'^register/', views.register, name='registration'),
+    # url(r'^login/', views.user_login, name='login'),
     url(r'^orders/', views.view_previous_orders, name='previous_orders'),
-    url(r'^$', views.visitors_main, name='visitors_main'),
+    url(r'^register/', views.register, name='registration'),
+    url(r'^$', views.visitors_main, name='index'),
 ]
