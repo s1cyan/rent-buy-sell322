ca147f128079958ff7d503a58fa488f0a1c16e6e
jnthnrzr <jonathanrozario@gmail.com>
2016-11-26 22:36:46 -0500

    added docstrings

--- rbs_app/rbs_application/forms.py
+++ rbs_app/rbs_application/forms.py
@@ -1,22 +1,14 @@
-# import re
+''' Forms for registration page.
+    Information grabbed: username, email, password
+
+    Thanks to original developer: Connie Liu '''
+
 from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 ''' CONNIE'S CODE
 class RegistrationForm(forms.Form):
-
-    username = forms.RegexField(
-    regex=r'^\w+$',
-    widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"),
-    error_messages={ 'invalid':_("This value must contain only letters, numbers and underscores.") })
-
-    email = forms.EmailField(
-    widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
-
-    password1 = forms.CharField(
-    widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)),
-    label=_("Password"))
 
     password2 = forms.CharField(
     widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)),
@@ -36,18 +28,17 @@ class RegistrationForm(forms.Form):
         return self.cleaned_data
 '''
 class UserForm(forms.Form):
-
+    ''' User registration form for RBS '''
     username = forms.RegexField(
-    regex=r'^\w+$',
-    widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("USERNAME *"),
-    error_messages={ 'invalid':_("This value must contain only letters, numbers and underscores.") })
-
+        regex=r'^\w+$', widget=forms.TextInput(
+        attrs=dict(required=True, max_length=30)), label=_("USERNAME *"), error_messages={ 'invalid':_("This value must contain only letters, numbers and underscores.") })
     email = forms.EmailField(
-    widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("EMAIL ADDRESS *"))
-
-    password = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("PASSWORD *"))
+        widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("EMAIL ADDRESS *"))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("PASSWORD *"))
 
     def clean_username(self):
+        ''' Clean form data for username '''
         try:
             user = User.objects.get(username__iexact=self.cleaned_data['username'])
         except User.DoesNotExist:
@@ -55,6 +46,7 @@ class UserForm(forms.Form):
         raise forms.ValidationError(_("The username already exists. Please try another one."))
 
     def clean(self):
+        ''' Clean form data for password '''
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
                 raise forms.ValidationError(_("Passwords did not match! Please Try Again."))
