36b266c4b4e17ad1aec5a4656f582ec41cf9cf64
Connie Liu <heyconnie123@gmail.com>
2016-11-28 22:40:43 -0500

    PositiveInteger Fix

--- rbs_app/rbs_application/models.py
+++ rbs_app/rbs_application/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 from mezzanine.core.fields import FileField
 
 class UserProfile(models.Model):
-    user_id = models.IntegerField(primary_key=True)
+    user_id = models.PositiveIntegerField(primary_key=True)
     user = models.OneToOneField(User, related_name='user')
     photo = FileField(verbose_name=_("Profile Picture"),
                       upload_to=upload_to("main.UserProfile.photo", "profiles"),
@@ -18,7 +18,7 @@ class UserProfile(models.Model):
     num_of_transactions = models.CharField(max_length=50, default='', blank=True)
     num_of_suspensions = models.CharField(max_length=50, default='', blank=True)
     status = models.TextField(default='', blank=True)
-    credit_card = models.IntegerField(max_length=19, blank=True, default='')
+    credit_card = models.PositiveIntegerField(max_length=19, blank=True, default='')
     products = models.ForeignKey(Product)
     def __str__(self):
         return self.user_id
@@ -29,15 +29,8 @@ class Ratings(models.Model):
     def __str__(self):
         return self.ratings
 
-def create_profile(sender, **kwargs):
-    user = kwargs["instance"]
-    if kwargs["created"]:
-        user_profile = UserProfile(user=user)
-        user_profile.save()
-post_save.connect(create_profile, sender=User)
-
 class Category(models.Model):
-    category_id = models.IntegerField(primary_key=True)
+    category_id = models.PositiveIntegerField(primary_key=True)
     category_name = models.CharField(max_length=30)
 
     class Meta:
@@ -47,12 +40,12 @@ class Category(models.Model):
         return self.category_id
 
 class Products(models.Model):
-    product_id = models.IntegerField(primary_key=True)
+    product_id = models.PositiveIntegerField(primary_key=True)
     category_id = models.ForeignKey(Category)
     category_name = models.CharField(Category)
     title = models.CharField(max_length=128)
     description = models.TextField()
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to="product_images", blank=True)
     status = models.CharField(max_length=100, blank=True)
@@ -65,14 +58,14 @@ class Products(models.Model):
 
 class Orders(models.Model):
     #assuming order number does not repeat...
-    order_id = models.IntegerField(primary_key=True)
+    order_id = models.PositiveIntegerField(primary_key=True)
     user_id = models.ForeignKey(UserProfile)
     product_id = models.ForeignKey(Products)
     def __str__(self):
         return self.order_id
 
 class Complaints(models.Model):
-    complaint_id = models.IntegerField(primary_key=True)
+    complaint_id = models.PositiveIntegerField(primary_key=True)
     pub_date = models.DateTimeField('date published', auto_now_add = True)
     start_date = models.DateField()
     end_date = models.DateField()
