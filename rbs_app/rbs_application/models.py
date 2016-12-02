b405083f89a00d39f849f6dfddf62f367fc37993
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2016-12-01 23:16:07 -0500

    deprecated imports

--- rbs_app/rbs_application/models.py
+++ rbs_app/rbs_application/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
