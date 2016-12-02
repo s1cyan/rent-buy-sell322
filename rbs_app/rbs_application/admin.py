71da6846ad576ca3a97f0def9a6f1964a59ad198
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2016-12-01 23:47:03 -0500

    switched to Connie's admin.py with all registered models

--- rbs_app/rbs_application/admin.py
+++ rbs_app/rbs_application/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import *
-
 # Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Product)
-admin.site.register(Category)
+
+myModels = [UserProfile, Rating, Category, Product, Order, Complaint, ShoppingCart]  # iterable list
+admin.site.register(myModels)
