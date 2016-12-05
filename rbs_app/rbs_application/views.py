67adac89b118848ebfc34ed32dc70f9a3a49ee80
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2016-12-05 11:11:26 -0500

    defining details view. Might have more bugs

--- rbs_app/rbs_application/views.py
+++ rbs_app/rbs_application/views.py
@@ -186,6 +186,7 @@ def process_sell(request):
 
 
 def show_results(request):
+    # Searching for products is handled by this function
     context_dict = dict()
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
@@ -206,47 +207,64 @@ def show_results(request):
 
 def details(request):
     # View for seeing item details
+    context_dict = dict()
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        context_dict['username'] = request.user.username
+        context_dict['money'] = profile.balance
+    product_pk = request.POST.get('pk', '')
+    product = Product.objects.get(pk=product_pk)
+    context_dict['item'] = product.title
+    context_dict['price'] = product.price
+    context_dict['seller'] = product.seller.username
+    # context_dict['option'] = 'n/a yet'
+    context_dict['description'] = product.text
+    context_dict['product_pk'] = product_pk
+    return render(request, 'user_item_details.html', context_dict)
+    # TODO NOBODY TOUCH THESE COMMENTED OUT CODES
+    # because multiple item can have the same name, access by pk
+    # if request.method == "POST":
+        # if request.user.is_authenticated: # need to check if its not auction, and if item is an auction item, redirect to an auction page
+            # GOing to the item details page
+            # if the item is clicked on, load the item details page with the Product information
+            # profile = UserProfile.objects.get(user=request.user)
+            # product_pk = request.POST.get('pk', '')
+            # product = Product.objects.get(pk=product_pk) # bc multiple item can have the same name, access by pk
+            # context_dict = {
+            #     'user': request.user.username,
+            #     'money': profile.balance,
+            #     'item': product.title,
+            #     'price': product.price,
+            #     'seller': product.seller.username,
+            #     'option': 'n/a yet', # TODO SET THE SELLING TYPE
+            #     'description': product.text,
+            #     'product_pk': product.pk
+            # }
+            # return HttpResponse("HERE")
+            # return render(request,'user_item_details.html',context_dict)
+        # else:
+        #     product_pk = request.POST.get('pk', '')
+        #     product = Product.objects.get(pk=product_pk)  # bc multiple item can have the same name, access by pk
+        #     context_dict = {
+        #         'item': product.title,
+        #         'price': product.price,
+        #         'seller': product.seller.username,
+        #         'option': 'n/a yet',  # TODO SET THE SELLING TYPE
+        #         'description': product.text,
+        #         'product_pk': product.pk
+        #     }
+        #     return render(request,'visitor_item_details.html',context_dict)
+
+    # return render(request, template, context_dict)
+    # needs catch statement if product.objects.get != search form...
+
+    # return render(request, template, context)
     pass
 
 
 
 
-        # if request.method == "POST":
-        #     if request.user.is_authenticated: # need to check if its not auction, and if item is an auction item, redirect to an auction page
-        #         # GOing to the item details page
-        #         # if the item is clicked on, load the item details page with the Product information
-        #         profile = UserProfile.objects.get(user=request.user)
-        #         product_pk = request.POST.get('pk', '')
-        #         product = Product.objects.get(pk=product_pk) # bc multiple item can have the same name, access by pk
-        #         context_dict = {
-        #             'user': request.user.username,
-        #             'money': profile.balance,
-        #             'item': product.title,
-        #             'price': product.price,
-        #             'seller': product.seller.username,
-        #             'option': 'n/a yet', # TODO SET THE SELLING TYPE
-        #             'description': product.text,
-        #             'product_pk': product.pk
-        #         }
-        #         # return HttpResponse("HERE")
-        #         return render(request,'user_item_details.html',context_dict)
-        #     else:
-        #         product_pk = request.POST.get('pk', '')
-        #         product = Product.objects.get(pk=product_pk)  # bc multiple item can have the same name, access by pk
-        #         context_dict = {
-        #             'item': product.title,
-        #             'price': product.price,
-        #             'seller': product.seller.username,
-        #             'option': 'n/a yet',  # TODO SET THE SELLING TYPE
-        #             'description': product.text,
-        #             'product_pk': product.pk
-        #         }
-        #         return render(request,'visitor_item_details.html',context_dict)
-        #
-        # return render(request, template, context_dict)
-    # needs catch statement if product.objects.get != search form...
 
-    # return render(request, template, context)
 
 
 @login_required
