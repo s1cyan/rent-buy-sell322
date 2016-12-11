bb53f805d4fa8fbec969478e9de5e8da2ec22a3c
Claudia Yan <claudiab.yan@gmail.com>
2016-12-11 18:24:46 -0500

    rating update math is fixed - had to cast for everything -_-

--- rbs_app/rbs_application/views/orders.py
+++ rbs_app/rbs_application/views/orders.py
@@ -59,8 +59,6 @@ def orders(request):
 def update_rating(seller_profile):
     rs = Rating.objects.filter(user = seller_profile)
     ratings = list(rs.values_list('rating', flat = True))
-    overall_rating = sum(ratings)/len(ratings)
-    print ("$$$$$ overall ", overall_rating)
-
+    overall_rating = float(float(sum(ratings))/float(len(ratings)))
     seller_profile.rbs_rating = Decimal(overall_rating)
     seller_profile.save()
