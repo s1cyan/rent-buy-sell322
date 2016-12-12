2b5a22a6eb3f09936ab3f9c35b5428acf46020c3
Connie Liu <heyconnie123@gmail.com>
2016-12-12 00:55:32 -0500

    vip user

--- rbs_app/rbs_application/views/update_account.py
+++ rbs_app/rbs_application/views/update_account.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.forms.models import inlineformset_factory
 from django.core.exceptions import PermissionDenied
-from ..forms import UserForm
+from ..forms import UserForm, UserForm_Django
 from ..models import UserProfile
 from ..date_checker import update_all
 
@@ -20,7 +20,7 @@ def update_account(request):
     user = User.objects.get(user=request.user.id)
     profile = UserProfile.objects.get(user=request.user)
 
-    user_form = UserForm(instance=user)
+    user_form = UserForm_Django(instance=user)
     ProfileInlineFormset = inlineformset_factory(User,
                                                  UserProfile,
                                                  fields=('bio',
@@ -30,17 +30,29 @@ def update_account(request):
                                                          'transactions',
                                                          'credit_card',
                                                          'strikes',
-                                                         'suspensions',))
+                                                         'suspensions',
+                                                         'rbs_vip_user'))
     formset = ProfileInlineFormset(instance=user)
 
     if request.user.is_authenticated() and request.user.id == user.id:
         if request.method == "POST":
-            user_form = UserForm(request.POST, request.FILES, instance=user)
+            user_form = UserForm_Django(request.POST, request.FILES, instance=user)
             formset = ProfileInlineFormset(request.POST, request.FILES or None, instance=user)
             if user_form.is_valid():
                 created_user = user_form.save(commit=False)
                 formset = ProfileInlineFormset(request.POST, request.FILES or None, instance=created_user)
                 if formset.is_valid():
+                    user = User.objects.get(user=request.user.id)
+                    profile = UserProfile.objects.get(user=request.user)
+                    if profile.balance >= 5000.00 and profile.transactions >= 5 and profile.suspensions == 0 and profile.strikes == 0:
+                        profile.rbs_vip_user = True
+                        print(profile.rbs_vip_user)
+                        print(profile.suspensions)
+                        print("i made it ---- true")
+                    else:
+                        profile.rbs_vip_user = False
+                        print("i made it --- false")
+                        print(profile.rbs_vip_user)
                     created_user.save()
                     formset.save()
                     return HttpResponseRedirect('/rbs/update')
