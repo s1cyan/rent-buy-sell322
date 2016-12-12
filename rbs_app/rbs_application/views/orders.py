74ca6700316306ea849e0cc1e97153682b36ed84
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2016-12-11 19:23:45 -0500

    Transactions somewhat works. Still buggy with showing not-up-to-date balance for current user

--- rbs_app/rbs_application/views/orders.py
+++ rbs_app/rbs_application/views/orders.py
@@ -8,10 +8,7 @@ from ..models import Order, ShoppingCart, UserProfile
 def orders(request):
     # Render the page for previous orders
     profile = UserProfile.objects.get(user=request.user)
-    context_dict = {
-        'username': request.user.username,
-        'money': profile.balance,
-    }
+    context_dict = dict()
     if "confirm_checkout" in request.POST:
         # TODO execute transactions, deduct from profile.balance
         # TODO deduct from quantities, and check to set product as inactive
@@ -20,16 +17,26 @@ def orders(request):
         new_order.save()
         new_order.products.add(*cart.products.all())
         for product in cart.products.all():
-            print("PRODUCT TO DELETE:", product)
+            seller = UserProfile.objects.get(user=product.seller)
+            print("\n\nTransaction for", product, "sold by", seller)
+            print("Seller balance BEFORE:", seller.balance)
+            print("Buyer balance BEFORE:", cart.user.balance)
+            seller.balance += product.price
+            seller.save()
+            cart.user.balance -= product.price
+            cart.user.save()
+            print("TRANSACTION DONE!!!!!!!!!!!!!!!!!")
+            print("Seller balance AFTER:", seller.balance)
+            print("Buyer balance BEFORE:", cart.user.balance)
             product.is_active = False
             product.save()
             print(product.is_active)
         cart_total = cart.products.all().aggregate(Sum('price'))
         amount = cart_total['price__sum']
-        print(amount)
-        profile.balance -= amount
-        profile.save()
-        print(profile.balance)
+        print("CART TOTAL was: $", amount)
+        # profile.balance -= amount
+        # profile.save()
+        # print(profile.balance)
         new_order.save()
         cart.delete()
     all_orders = Order.objects.filter(user=profile)
@@ -41,5 +48,6 @@ def orders(request):
             product_list = list(order.products.all())
             order_dic[str(order.pk)] = product_list
         context_dict['allorders'] = order_dic
+    context_dict['username'] = request.user.username
+    # context_dict['money'] = profile.balance
     return render(request, 'orders.html', context_dict)
-
