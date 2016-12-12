e6820f3da70ec0fc1d033d8a93759f5f11cf5b20
Claudia Yan <claudiab.yan@gmail.com>
2016-12-11 23:15:26 -0500

    Validating for a bot - without adding in captcha

--- rbs_app/rbs_application/views/register.py
+++ rbs_app/rbs_application/views/register.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from ..forms import UserForm
 from ..date_checker import update_all
-
+from random import randint
 
 def register(request):
     """Registration Page View
@@ -15,10 +15,20 @@ def register(request):
     registered = False
     # If it's a HTTP POST, we're interested in processing form data.
     update_all()
+    x = randint(0,9)
+    y = randint(0,9)
+    math_equation = str(x) +'+' + str(y) + '='
+    context_dict= {
+        'math': math_equation
+    }
     if request.method == 'POST':
         # Attempt to grab information from the raw form information.
         user_form = UserForm(data=request.POST)
-        if user_form.is_valid(): # and registration_form.is_valid():
+        eq = request.POST.get('eq','')
+        form_ans = request.POST.get('answer')
+        ans = int( eq[0]) + int (eq[2])
+
+        if user_form.is_valid() and int(form_ans) == int(ans) : # and registration_form.is_valid():
             user = User.objects.create_user(
                 first_name=user_form.cleaned_data['first_name'],
                 last_name=user_form.cleaned_data['last_name'],
@@ -35,4 +45,4 @@ def register(request):
     # Render the template depending on the context.
     return render(request, 'registration.html',
                   {'user_form': user_form,
-                   'registered': registered})
+                   'registered': registered, 'math': math_equation})
